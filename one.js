bb0c6bc8b4d46a7f91ab8110dcdea80d883845ac
zhuye-yichu <82937065+zhuye-yichu@users.noreply.github.com>
2022-09-20 14:02:40 +0800

    js

--- one.js
+++ one.js
@@ -1,148 +1,3 @@
-var myClass2 = {//按照数组的某个属性排序
-	compare:function(property) {
-	  return function (a, b) {
-	    var val1 = a[property]
-	    var val2 = b[property]
-	    return val1 - val2
-	  }
-	}
-}
-var myClass = {
-	randomNum: function(rd){//规定范围随机数
-		return Math.floor(Math.random()*(rd)+1)
-	},
-	randomNum_qz: function(arr_value1,arr_value2,value1_sum){//权重随机数返回
-		let mer = 0,i = 0
-		// alert("value1_sum："+value1_sum+','+"arr_value1："+arr_value1+"max："+max)
-		
-		// while(i<value1_sum){	
-			i++
-			let left = 0, right = arr_value1.length-1
-			var random = Math.floor(Math.random()*value1_sum)+1
-			// alert("random："+random)
-			while(left<right){			
-				const mid = Math.floor((right - left) / 2) + left;
-				// alert("mid："+mid)
-				if(arr_value1[mid]<random){
-					left = mid+1
-				}
-				else{
-					right = mid
-				}
-			}
-			// alert("left:"+left)
-			mer = left
-		// }		
-		return arr_value2[mer]
-	},
-	randomNum_fw: function(min,max){//规定范围随机数
-		return Math.floor(min+Math.random()*((max+1)-min))
-	},
-	parnum: function(num,rd,sub){//单次抽样
-		let a = new Array()
-		let i = 0,b = 0
-		var pd = true
-		// a[0] = this.randomNum(rd)
-		while(i<num){
-			i = i+1
-			if(sub==="repeat"){
-				a[i] = this.randomNum(rd)
-			}
-			else{
-				//这里需要验证一下，num不能大于rd
-				pd = true
-				while(pd){
-					b = this.randomNum(rd)
-					pd = false
-					for(var j=0;j<=i;j++){
-						if(b===a[j])
-							pd = true
-					}
-				}
-				a[i] = b	
-			}	
-		}
-		a = a.slice(1).join()
-		return a
-	},
-	parnum_qz: function(num,rd,sub,arr_value1,arr_value2,value1_sum){//权重的单次抽样	
-		let a = new Array()
-		let i = 0,b = 0
-		var pd = true
-		// alert('num:'+num+'')
-		while(i<num){
-			i = i+1
-			if(sub==="repeat"){
-				a[i] = this.randomNum_qz(arr_value1,arr_value2,value1_sum)
-			}
-			else{
-				pd = true
-				while(pd){
-					b = this.randomNum_qz(arr_value1,arr_value2,value1_sum)
-					pd = false
-					for(var j=0;j<=i;j++){
-						if(b===a[j])
-							pd = true
-					}
-				}
-				a[i] = b	
-			}	
-		}
-		a = a.slice(1).join()
-		return a
-		
-	},
-	parnum_fw: function(min,max,num,sub){//单次抽样
-		let a = new Array()
-		let i = 0,b = 0
-		var pd = true
-		while(i<num){
-			i = i+1
-			if(sub==="repeat"){
-				a[i] = this.randomNum_fw(min,max)
-			}
-			else{
-				pd = true
-				while(pd){
-					b = this.randomNum_fw(min,max)
-					pd = false
-					for(var j=0;j<=i;j++){
-						if(b===a[j])
-							pd = true
-					}
-				}
-				a[i] = b	
-			}	
-		}
-		a = a.slice(1).join()
-		return a
-	},
-	//多次抽样列表
-	parfre:function(tool,fre,num,rd,sub,arr_value1,arr_value2,value1_sum){//多次抽样列表
-		// alert(fre + ','+num+','+rd)
-		let arr =new Array()
-		for (let i=0;i<fre;i++){
-			switch(tool){
-				case 1:
-					arr[i] = 'Fre' + (i+1) + 'Rd' + num + '：' + this.parnum(num,rd,sub)
-					break;
-				case 2:
-					arr[i] = 'Fre' + (i+1) + 'Rd' + num + '：' + this.parnum_qz(num,rd,sub,arr_value1,arr_value2,value1_sum)
-					break;
-			}
-		}
-		return arr
-	},
-	parfre3:function(tool,min,max,num,fre,sub){
-		let arr =new Array()
-		for (let i=0;i<fre;i++){
-			arr[i] = 'Fre' + (i+1) + 'Rd' + num + '：' + this.parnum_fw(min,max,num,sub)
-		}
-		return arr
-	}
-}
-
-	
 Vue.config.productionTip = false 
 //阻止vue在启动时生成提示
 
@@ -206,8 +61,8 @@ const vm = new Vue({
 		//工具1
 		calResultTool1(){//计算结果
 			if(inputcheck_pub1(this.rd1,this.num1,this.fre1,this.sub1)){
-				let arr = new Array(),tool=1
-				arr = myClass.parfre(tool,this.fre1,this.num1,this.rd1,this.sub1)
+				let arr = new Array()
+				arr = myClass.parfre1(this.fre1,this.num1,this.rd1,this.sub1)
 				this.resultTool1 = arr	
 				document.getElementById("result_box1").scrollIntoView()
 			}
@@ -232,19 +87,17 @@ const vm = new Vue({
 			if(!inputcheck_pub2(this.num2,this.sub2,this.native.length)){				
 				alert('当选择为不重复时，num 不能大于 数字或备选词 的个数')
 				return false
-			}
-			//将数组对象由权重大小进行排序
-			var arr_native = this.native.sort(myClass2.compare("value1"))
+			}			
 			//进行遍历取值
-			var arr_value1 = [],arr_value2 = []
-			var value1_sum = 0 //权重数==后备词数组长度 权重总和
-			arr_native.forEach(function(item,index){
-				arr_value1.push(item.value1)
-				value1_sum = value1_sum+item.value1
-				arr_value2.push(item.value2)
+			var arr_value=[]			
+			this.native.forEach(function(item,index){
+				for(var i=0;i<item.value1;i++){
+					arr_value.push(item.value2)
+				}
 			})
-			let arr = new Array(),tool = 2
-			arr = myClass.parfre(tool,this.fre2,this.num2,this.rd2,this.sub2,arr_value1,arr_value2,value1_sum)
+			var value_sum = arr_value.length //权重数==后备词数组长度 权重总和
+			let arr = new Array()
+			arr = myClass.parfre2(this.fre2,this.num2,this.sub2,arr_value,value_sum)
 			this.resultTool2 = arr	
 			document.getElementById("result_box2").scrollIntoView()
 		},
